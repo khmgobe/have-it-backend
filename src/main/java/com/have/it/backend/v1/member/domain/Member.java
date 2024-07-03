6a3627105cf416ab7413030cef6cc3e9b09ba16f
khmgobe <dr94406@naver.com>
2024-07-03 18:22:10 +0900

    feat: 멤버 엔티티 데이터 생성

---
+++ src/main/java/com/have/it/backend/v1/member/domain/Member.java
@@ -0,0 +1,53 @@
+package com.have.it.backend.v1.member.domain;
+
+import com.have.it.backend.v1.common.util.BaseException;
+import com.have.it.backend.v1.common.util.enumeration.ExceptionInformation;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+
+    private static final int MAX_NICKNAME_LENGTH = 15;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+
+    @Builder
+    private Member(Long id, String email, String nickname, String password) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+    }
+
+    public void updateNickname(final String nickname) {
+        validateNickname(nickname);
+        this.nickname = nickname;
+    }
+
+    private void validateNickname(final String nickname) {
+        if(Objects.isNull(nickname) || nickname.length() > MAX_NICKNAME_LENGTH) {
+            throw new BaseException(ExceptionInformation.ID_NOT_FOUND);
+        }
+    }
+}
