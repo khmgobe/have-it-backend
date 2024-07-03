d381f9db1b139ca2db2fe3e2af03ce5bf0bb0918
khmgobe <dr94406@naver.com>
2024-07-03 18:22:16 +0900

    feat: 포스트 엔티티 데이터 생성

---
+++ src/main/java/com/have/it/backend/v1/post/domain/Post.java
@@ -0,0 +1,27 @@
+package com.have.it.backend.v1.post.domain;
+
+import com.have.it.backend.v1.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Member member;
+
+}
