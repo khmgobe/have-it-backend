b15991d05dafc0d5947f29667c0abe4e2926a5c4
khmgobe <dr94406@naver.com>
2024-07-05 18:00:52 +0900

    refactor : 조회 반환 데이터 추가

--- src/main/java/com/have/it/backend/v1/member/domain/dto/response/MemberReadResponse.java
+++ src/main/java/com/have/it/backend/v1/member/domain/dto/response/MemberReadResponse.java
@@ -7,12 +7,14 @@ import lombok.Getter;
 @Getter
 public class MemberReadResponse {
 
+    private final Long id;
     private final String nickname;
     private final String email;
 
 
     @Builder
-    private MemberReadResponse(String nickname, String email) {
+    private MemberReadResponse(Long id, String nickname, String email) {
+        this.id = id;
         this.nickname = nickname;
         this.email = email;
     }
@@ -20,6 +22,7 @@ public class MemberReadResponse {
     public static MemberReadResponse from(Member member) {
         return MemberReadResponse
                 .builder()
+                .id(member.getId())
                 .email(member.getEmail())
                 .nickname(member.getNickname())
                 .build();
