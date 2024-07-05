936beaa3719d6aad36e17349b1b3dde369ebc34f
khmgobe <dr94406@naver.com>
2024-07-05 17:56:18 +0900

    chore : 불필요 코드 삭제

--- src/main/java/com/have/it/backend/v1/member/domain/dto/response/MemberCreateResponse.java
+++
@@ -1,7 +0,0 @@
-package com.have.it.backend.v1.member.domain.dto.response;
-
-import lombok.Getter;
-
-@Getter
-public class MemberCreateResponse {
-}
--- src/main/java/com/have/it/backend/v1/member/domain/dto/response/MemberDeleteResponse.java
+++
@@ -1,9 +0,0 @@
-package com.have.it.backend.v1.member.domain.dto.response;
-
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public class MemberDeleteResponse {
-}
