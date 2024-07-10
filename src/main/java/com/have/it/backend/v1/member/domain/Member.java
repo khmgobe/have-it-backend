41302ea0dac177fa2774255b3351642952894f7f
khmgobe <dr94406@naver.com>
2024-07-10 16:13:54 +0900

    refactor: 도메인 객체 리네이밍

--- src/main/java/com/have/it/backend/v1/member/domain/Member.java
+++ src/main/java/com/have/it/backend/v1/member/domain/Member.java
@@ -1,17 +1,36 @@
 package com.have.it.backend.v1.member.domain;
 
-import com.have.it.backend.v1.member.domain.dto.request.MemberCreateRequest;
+import com.have.it.backend.v1.common.util.BaseException;
+import com.have.it.backend.v1.common.util.enumeration.ExceptionInformation;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
 
 @Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
 
-    private final Long id;
-    private final String email;
-    private final String nickname;
-    private final String password;
+    private static final int MAX_NICKNAME_LENGTH = 15;
+    private static final int MAX_EMAIL_LENGTH = 30;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
 
 
     @Builder
@@ -22,18 +41,33 @@ public class Member {
         this.password = password;
     }
 
-    public void updateNickname(String nickname) {
-        Member
+    public static Member of(String email, String nickname, String password) {
+        return Member
                 .builder()
+                .email(email)
                 .nickname(nickname)
+                .password(password)
                 .build();
     }
 
-    public static Member of(String email, String nickname, String password) {
+    public void updateNickname(final String nickname) {
+        validateNickname(nickname);
+        this.nickname = nickname;
+    }
 
-        return Member.builder()
-                .email(email)
-                .nickname(nickname)
-                .password(password).build();
+    private void validateNickname(final String nickname) {
+        if(Objects.isNull(nickname) || nickname.length() > MAX_NICKNAME_LENGTH) {
+            throw new BaseException(ExceptionInformation.MEMBER_NICKNAME_ILLEGAL_LENGTH);
+        }
+    }
+
+    public static Member fromModel(Member member) {
+        return Member
+                .builder()
+                .id(member.getId())
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .password(member.getPassword())
+                .build();
     }
 }
