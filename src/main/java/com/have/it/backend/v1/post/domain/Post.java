af3ba5c2056cdf87fa87dfd39ed091279af57b18
khmgobe <dr94406@naver.com>
2024-07-10 16:17:48 +0900

    refactor: 게시 객체 변경

--- src/main/java/com/have/it/backend/v1/post/domain/Post.java
+++ src/main/java/com/have/it/backend/v1/post/domain/Post.java
@@ -1,41 +1,61 @@
 package com.have.it.backend.v1.post.domain;
 
 import com.have.it.backend.v1.member.domain.Member;
-import com.have.it.backend.v1.post.domain.dto.request.PostCreateRequest;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
 
-    private final Long id;
-    private final String title;
-    private final String content;
-    private final Member member;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Member writer;
 
     @Builder
-    private Post(Long id, String title, String content, Member member) {
+    private Post(Long id, String title, String content, Member writer) {
         this.id = id;
         this.title = title;
         this.content = content;
-        this.member = member;
+        this.writer = writer;
     }
 
-    public static Post of(PostCreateRequest request, Member member) {
+    public static Post fromModel(Post post) {
         return Post
                 .builder()
-                .title(request.getTitle())
-                .content(request.getContent())
-                .member(member)
+                .id(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .writer(post.getWriter())
                 .build();
     }
 
-    public void update(String title, String content) {
-
-        Post.builder()
+    public static Post of(String title, String content, Member member) {
+        return Post
+                .builder()
                 .title(title)
                 .content(content)
+                .writer(member)
                 .build();
+    }
+
+     public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }
