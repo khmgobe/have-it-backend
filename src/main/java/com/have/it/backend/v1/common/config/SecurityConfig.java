55762fb690ec33b41af73d3cbdf6c46bb60f4f99
khmgobe <dr94406@naver.com>
2024-07-10 20:06:54 +0900

    feat: SecurityConfig, Security 의존성 추가

---
+++ src/main/java/com/have/it/backend/v1/common/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.have.it.backend.v1.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)      // CSRF 보호 기능 비활성화
+                .logout(AbstractHttpConfigurer::disable)    // 기본 인증 로그아웃 비활성화
+                .formLogin(AbstractHttpConfigurer::disable) // 기본 로그인 폼 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable) // 기본 인증 로그인 비활성화
+                .sessionManagement(param -> param.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용 비활성화
+
+                // request 설정
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(new AntPathRequestMatcher("/api/v1/member/**"),
+                                         new AntPathRequestMatcher("api/v1/post/**"))
+                        .permitAll().anyRequest().authenticated());
+
+        return http.build();
+    }
+}
