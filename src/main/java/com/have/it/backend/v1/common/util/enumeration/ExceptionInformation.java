5bd952e47e199f5f9617cea859cd963db5d93631
khmgobe <dr94406@naver.com>
2024-07-10 20:08:40 +0900

    chore: 예외 응답 객체 주석 추가

--- src/main/java/com/have/it/backend/v1/common/util/enumeration/ExceptionInformation.java
+++ src/main/java/com/have/it/backend/v1/common/util/enumeration/ExceptionInformation.java
@@ -12,9 +12,14 @@ public enum ExceptionInformation {
     /* 204: NO_CONTENT */
     ID_NO_CONTENT("아이디가 존재하지 않습니다", HttpStatus.NOT_FOUND.value()),
 
-    MEMBER_NICKNAME_ILLEGAL_LENGTH("올바르지 않은 닉네임 길이입니다.", HttpStatus.BAD_REQUEST.value()),
-    MEMBER_EMAIL_ILLEGAL_LENGTH("올바르지 않은 이메일 길이입니다.", HttpStatus.BAD_REQUEST.value()),
-    MEMBER_EMAIL_DUPLICATED("이메일이 중복되었습니다.", HttpStatus.CONFLICT.value());
+    /* 400: BAD_REQUEST */
+    ID_OR_PASSWORD_ILLEGAL_TEXT("아이디 또는 비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST.value()),
+    NICKNAME_ILLEGAL_LENGTH("올바르지 않은 닉네임 길이입니다.", HttpStatus.BAD_REQUEST.value()),
+    EMAIL_ILLEGAL_LENGTH("올바르지 않은 이메일 길이입니다.", HttpStatus.BAD_REQUEST.value()),
+    PASSWORD_ILLEGAL_LENGTH("올바르지 않은 비밀번호 길이입니다.", HttpStatus.BAD_REQUEST.value()),
+
+    /* 409: CONFLICT */
+    EMAIL_DUPLICATED("이메일이 중복되었습니다.", HttpStatus.CONFLICT.value());
 
     private final String message;
     private final Integer status;
