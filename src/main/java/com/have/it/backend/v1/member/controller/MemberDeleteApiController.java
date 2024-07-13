03c8d9e2ab52ab67b95b43c1b5cf00ebfceb9c22
khmgobe <dr94406@naver.com>
2024-07-13 18:32:02 +0900

    refactor: 의존 구조 변경 및 코드 수정

--- src/main/java/com/have/it/backend/v1/member/controller/MemberDeleteApiController.java
+++ src/main/java/com/have/it/backend/v1/member/controller/MemberDeleteApiController.java
@@ -1,7 +1,7 @@
 package com.have.it.backend.v1.member.controller;
 
 import com.have.it.backend.v1.common.util.BaseResponse;
-import com.have.it.backend.v1.member.service.MemberDeleteService;
+import com.have.it.backend.v1.member.service.usecase.MemberDeleteUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MemberDeleteApiController {
 
-    private final MemberDeleteService service;
+    private final MemberDeleteUseCase memberDeleteUseCase;
 
 
     @DeleteMapping("/api/v1/member/delete/{memberId}")
     public ResponseEntity<BaseResponse<Long>> deleteMember (@PathVariable Long memberId) {
 
-        service.deleteMember(memberId);
+        memberDeleteUseCase.deleteMember(memberId);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
--- src/main/java/com/have/it/backend/v1/member/service/MemberDeleteService.java
+++ src/main/java/com/have/it/backend/v1/member/service/MemberDeleteService.java
@@ -3,6 +3,7 @@ package com.have.it.backend.v1.member.service;
 import com.have.it.backend.v1.common.util.BaseException;
 import com.have.it.backend.v1.common.util.enumeration.ExceptionInformation;
 import com.have.it.backend.v1.member.repository.MemberJpaRepository;
+import com.have.it.backend.v1.member.service.usecase.MemberDeleteUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,15 +11,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberDeleteService {
+public class MemberDeleteService implements MemberDeleteUseCase {
 
     private final MemberJpaRepository repository;
 
+    @Override
     public void deleteMember(final Long memberId) {
 
-        repository
-                .findById(memberId)
-                .orElseThrow(() -> new BaseException(ExceptionInformation.ID_NO_CONTENT));
+        repository.findById(memberId)
+                  .orElseThrow(() -> new BaseException(ExceptionInformation.ID_NO_CONTENT));
 
         repository.deleteById(memberId);
 
