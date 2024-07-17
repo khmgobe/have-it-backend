e5ed83b1478787fdd3a12ebe365ed19f2c1740d5
khmgobe <dr94406@naver.com>
2024-07-17 14:49:15 +0900

    chore: 게시 도메인 테스트 서포트 MockBean 추가

--- src/test/java/com/have/it/backend/util/ApiControllerTestSupport.java
+++ src/test/java/com/have/it/backend/util/ApiControllerTestSupport.java
@@ -10,6 +10,14 @@ import com.have.it.backend.v1.member.service.usecase.MemberCreateUseCase;
 import com.have.it.backend.v1.member.service.usecase.MemberDeleteUseCase;
 import com.have.it.backend.v1.member.service.usecase.MemberReadUseCase;
 import com.have.it.backend.v1.member.service.usecase.MemberUpdateUseCase;
+import com.have.it.backend.v1.post.controller.PostCreateApiController;
+import com.have.it.backend.v1.post.controller.PostDeleteApiController;
+import com.have.it.backend.v1.post.controller.PostReadApiController;
+import com.have.it.backend.v1.post.controller.PostUpdateApiController;
+import com.have.it.backend.v1.post.service.usecase.PostCreateUseCase;
+import com.have.it.backend.v1.post.service.usecase.PostDeleteUseCase;
+import com.have.it.backend.v1.post.service.usecase.PostReadUseCase;
+import com.have.it.backend.v1.post.service.usecase.PostUpdateUseCase;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +29,11 @@ import org.springframework.test.web.servlet.MockMvc;
         MemberCreateApiController.class,
         MemberReadApiController.class,
         MemberUpdateApiController.class,
-        MemberDeleteApiController.class
+        MemberDeleteApiController.class,
+        PostCreateApiController.class,
+        PostReadApiController.class,
+        PostUpdateApiController.class,
+        PostDeleteApiController.class
 })
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public abstract class ApiControllerTestSupport {
@@ -44,4 +56,16 @@ public abstract class ApiControllerTestSupport {
 
     @MockBean
     protected MemberDeleteUseCase memberDeleteUseCase;
+
+    @MockBean
+    protected PostCreateUseCase postCreateUseCase;
+
+    @MockBean
+    protected PostReadUseCase postReadUseCase;
+
+    @MockBean
+    protected PostUpdateUseCase postUpdateUseCase;
+
+    @MockBean
+    protected PostDeleteUseCase postDeleteUseCase;
 }
