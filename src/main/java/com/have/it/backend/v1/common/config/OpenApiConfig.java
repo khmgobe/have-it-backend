a5c25e246b9ab60f4091f729ab90541ecdc371ab
khmgobe <dr94406@naver.com>
2024-07-22 17:45:32 +0900

    chore: 불필요 정보 수정

--- src/main/java/com/have/it/backend/v1/common/config/OpenApiConfig.java
+++ src/main/java/com/have/it/backend/v1/common/config/OpenApiConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.http.HttpHeaders;
 import java.util.Collections;
@@ -15,9 +14,6 @@ public class OpenApiConfig {
 
     private static final String API_TITLE = "Have-it 백엔드 서버 문서";
     private static final String API_DESCRIPTION = "";
-    private static final String API_TERMS_OF_SERVICE = "https://swagger.io/terms/";
-    private static final String API_LICENSE_NAME = "Apache 2.0";
-    private static final String API_LICENSE_URL = "https://springdoc.org";
     private static final String SERVER_URL = "/";
     private static final String SERVER_DESCRIPTION = "현재 위치한 서버";
     private static final String SECURITY_SCHEME_NAME = "bearerAuth";
@@ -40,10 +36,6 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title(API_TITLE)
                         .version(appVersion)
-                        .description(API_DESCRIPTION)
-                        .termsOfService(API_TERMS_OF_SERVICE)
-                        .license(new License()
-                                .name(API_LICENSE_NAME)
-                                .url(API_LICENSE_URL)));
+                        .description(API_DESCRIPTION));
     }
 }
