33e5770b75a40acce43917e511e1b25d55102c7d
khmgobe <dr94406@naver.com>
2024-07-23 23:07:23 +0900

    refactor: 리퀘스트에 권한 값 추가 및 유효성 처리

--- src/main/java/com/have/it/backend/v1/member/dto/request/MemberCreateRequest.java
+++ src/main/java/com/have/it/backend/v1/member/dto/request/MemberCreateRequest.java
@@ -1,18 +1,24 @@
 package com.have.it.backend.v1.member.dto.request;
 
+import com.have.it.backend.v1.member.domain.enumeration.Role;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record MemberCreateRequest (
-                    @NotBlank(message = "이메일은 비어있을 수 없습니다.")
+        @NotBlank(message = "이메일은 비어있을 수 없습니다.")
                     String email,
 
-                    @NotBlank(message = "닉네임은 비어있을 수 없습니다.")
+        @NotBlank(message = "닉네임은 비어있을 수 없습니다.")
                     String nickname,
 
-                    @NotBlank(message = "비밀번호는 비어있을 수 없습니다.")
-                    String password){
+        @NotBlank(message = "비밀번호는 비어있을 수 없습니다.")
+                    String password,
+
+        @NotNull(message = "권한은 비어있을 수 없습니다.")
+                    Role role
+        ) {
 
 
     public MemberCreateRequest toServiceRequest() {
@@ -21,6 +27,7 @@ public record MemberCreateRequest (
                 .email(email)
                 .nickname(nickname)
                 .password(password)
+                .role(role)
                 .build();
     }
 }
