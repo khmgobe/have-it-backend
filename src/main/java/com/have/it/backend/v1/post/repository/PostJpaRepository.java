06f4c9307e7108079ac855f5d409e511993d0fea
khmgobe <dr94406@naver.com>
2024-07-23 17:44:06 +0900

    feat: fetch join 추가

--- src/main/java/com/have/it/backend/v1/post/repository/PostJpaRepository.java
+++ src/main/java/com/have/it/backend/v1/post/repository/PostJpaRepository.java
@@ -2,6 +2,17 @@ package com.have.it.backend.v1.post.repository;
 
 import com.have.it.backend.v1.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PostJpaRepository extends JpaRepository<Post, Long> {
-}
+
+        @Query("SELECT p FROM Post p JOIN FETCH p.writer w")
+        List<Post> findAllWithWriter();
+
+        @Query("SELECT p FROM Post p JOIN FETCH p.writer WHERE p.id = :postId")
+        Optional<Post> findByIdWithWriter(@Param("postId") Long postId);
+    }
