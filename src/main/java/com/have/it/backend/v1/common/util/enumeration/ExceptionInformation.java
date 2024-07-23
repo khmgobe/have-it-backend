d02c6ff80e7620d04dd714504386feb6cbeac5f2
khmgobe <dr94406@naver.com>
2024-07-23 23:06:18 +0900

    chore: 예외 생성에 필요한 열거형 값 추가

--- src/main/java/com/have/it/backend/v1/common/util/enumeration/ExceptionInformation.java
+++ src/main/java/com/have/it/backend/v1/common/util/enumeration/ExceptionInformation.java
@@ -15,7 +15,10 @@ public enum ExceptionInformation {
     /* 400: BAD_REQUEST */
     ID_OR_PASSWORD_ILLEGAL_TEXT("아이디 또는 비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST.value()),
     NICKNAME_ILLEGAL_LENGTH("올바르지 않은 닉네임 길이입니다.", HttpStatus.BAD_REQUEST.value()),
+    NICKNAME_ILLEGAL_NULL("올바르지 않은 닉네임 값입니다.", HttpStatus.BAD_REQUEST.value()),
+    ROLE_ILLEGAL_NULL("올바르지 않은 권한 값입니다.", HttpStatus.BAD_REQUEST.value()),
     EMAIL_ILLEGAL_LENGTH("올바르지 않은 이메일 길이입니다.", HttpStatus.BAD_REQUEST.value()),
+    EMAIL_ILLEGAL_PATTERN("올바르지 않은 이메일 패턴입니다.", HttpStatus.BAD_REQUEST.value()),
     PASSWORD_ILLEGAL_LENGTH("올바르지 않은 비밀번호 길이입니다.", HttpStatus.BAD_REQUEST.value()),
 
     /* 409: CONFLICT */
